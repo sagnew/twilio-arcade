938ec3676873cb0851964de5fe0f16f6c93359ca
Sam Agnew <sagnew@eden.rutgers.edu>
2018-06-11 15:48:20 -0600

    Slight code cleanup and comments

--- index.js
+++ index.js
@@ -8,23 +8,24 @@ const twilio = require('twilio');
 
 const app = express();
 
-const gameboyButtons = ['a', 'b', 'x', 'y', 'left', 'right', 'up', 'down', 'start', 'select'];
-const keys = ['select', 'a', 'up', 'x', 'left', 'start', 'right', 'b', 'down', 'y'];
-const playerTwoNumbers = ['+14154032748', '+441618505031']
+const buttons = ['select', 'a', 'up', 'x', 'left', 'start', 'right', 'b', 'down', 'y'];
+
+// Replace these with whatever numbers you want to control Player Two.
+// I have an American and British number from when we used this at SIGNAL London.
+const playerTwoNumbers = ['+19145952857', '+447492882126']
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.post('/handle-key', (req, res) => {
   let digitPressed = req.body.Digits;
   let number = req.body.To;
-  let button = keys[digitPressed];
+  let button = buttons[digitPressed];
 
-  if(gameboyButtons.indexOf(button) > -1) {
+  if(buttons.indexOf(button) > -1) {
     if(['a', 'b', 'x', 'y'].includes(button)) {
       button = button.toUpperCase();
     }
 
-    console.log(button);
     if(playerTwoNumbers.includes(number)) {
       fs.writeFile('p2.txt', button, 'utf8', () => {});
     } else {
@@ -37,23 +38,23 @@ app.post('/handle-key', (req, res) => {
     numDigits: '1',
     action: '/handle-key',
     method: 'POST',
+    timeout: '9999'
   });
 
   res.send(twiml.toString());
 });
 
 app.post('/voice', (req, res) => {
-  console.log(req.body.Body);
-
   let twiml = new twilio.TwimlResponse();
   twiml.say('Connected. Refer to the instructions on screen for which buttons to press.');
   twiml.gather({
     numDigits: '1',
     action: '/handle-key',
     method: 'POST',
+    timeout: '9999'
   });
 
   res.send(twiml.toString());
 });
 
-app.listen(5000, console.log('Listening on port 5000'));
+app.listen(3000, console.log('Listening on port 3000'));
